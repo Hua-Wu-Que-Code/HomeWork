de99319d918aa3ce61f720c989ca2fc16f534c21
花无缺 <1178306549@qq.com>
2021-12-29 19:08:03 +0800

    更正了一些错误

--- Vue/期末作业/code/springboot/src/main/java/com/example/springboot/entity/Collection.java
+++ Vue/期末作业/code/springboot/src/main/java/com/example/springboot/entity/Collection.java
@@ -2,9 +2,13 @@ package com.example.springboot.entity;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Collection {
     private Integer id;
     private Integer bid;
     private Integer uid;
+
+    private Book book;
 }
